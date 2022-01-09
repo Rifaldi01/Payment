2b4a92a9a86bb6df14c8b2b1310f0f2321d145a2
Rifaldi01 <rifaldipratama2004@gmail.com>
2022-01-08 22:28:32 -1100

    event

--- resources/views/admin/event/create.blade.php
+++ resources/views/admin/event/create.blade.php
@@ -10,6 +10,17 @@
                     <input type="text" class="form-control" id="validationFormCheck1" placeholder="Name Event" required="">
                     <div class="invalid-feedback">Example invalid feedback text</div>
                 </div>
+
+                <label for="select" class="form-label "><i class="text-danger">*</i>From Company</label>
+                <div class="mb-3">
+                    <select class="form-select" required="" aria-label="select example">
+                        <option value="">Select Company...</option>
+                        <option value="1">PT. MAKMUR </option>
+                        <option value="2">PT. PERKASA </option>
+                        <option value="3">PT. SEJAHTERA INDONESIA</option>
+                    </select>
+                </div>
+
                 <div class="row g-2 mb-2">
                     <div class="col-md-6">
                         <label class="form-label" ><i class="text-danger">*</i>Start Event</label>
@@ -23,28 +34,25 @@
                     </div>
                 </div>
 
-                <label for="select" class="form-label mt-2"><i class="text-danger">*</i>Public To :</label>
-                <div class="mb-3">
-                    <select class="form-select" required="" aria-label="select example">
-                        <option value="">Public To...</option>
-                        <option value="1">Facebook</option>
-                        <option value="2">Instagram</option>
-                        <option value="3">Twitter</option>
-                    </select>
+                <div>
+                    <label  class="form-label"><i class="text-danger">*</i>Budget</label>
+                    <input class="form-control" type="number" placeholder="Masukan Budget" required="">
                 </div>
-
-                <label for="select" class="form-label "><i class="text-danger">*</i>From Company</label>
-                <div class="mb-3">
-                    <select class="form-select" required="" aria-label="select example">
-                        <option value="">Select Company...</option>
-                        <option value="1">PT. MAKMUR </option>
-                        <option value="2">PT. PERKASA </option>
-                        <option value="3">PT. SEJAHTERA INDONESIA</option>
-                    </select>
+                <div>
+                    <label  class="form-label mb-2 mt-2"><i class="text-danger">*</i>Max Post</label>
+                    <input class="form-control" type="number" aria-label="file example" placeholder="Maksimal Post" required="">
+                </div>
+                <div>
+                    <label class="form-label mt-2"><i class="text-danger">*</i>Description</label>
+                    <textarea name="" id="" cols="30" rows="10" class="form-control"  required></textarea>
+                </div>
+                <div>
+                    <label  class="form-label  mt-2"><i class="text-danger">*</i>Hastag</label>
+                    <input class="form-control" type="text" placeholder="Masukan Hastag" required="">
                 </div>
                 <div>
                     <label for="select" class="form-label">Image Event</label>
-                    <input class="form-control" type="file" aria-label="file example" required="">
+                    <input class="form-control" type="file" aria-label="file example"  required="">
                 </div>
 
                 <div>
